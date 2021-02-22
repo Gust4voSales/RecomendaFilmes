b8051b36e26f1fea96f9545adcdc5a625f4d9e4c
Gust4voSales <manoel0gustavo@gmail.com>
2021-02-22 15:03:15 -0300

    fix: remove Header back button custom behavior

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,32 +1,21 @@
 import React from 'react';
 import { MdArrowBack } from 'react-icons/md';
-import { Link, useHistory } from 'react-router-dom';
+import { Link, } from 'react-router-dom';
 import icon from '../../assets/icon.png';
 import './styles.scss';
 
 interface HeaderProps {
   backButtonRoute: string;
-  goBack?: boolean;
 }
 
-const Header: React.FC<HeaderProps> = ({ backButtonRoute, goBack=false }) => {
-  const history = useHistory();
-
-  function handleBackClick(e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) {
-    if (goBack) {
-      e.preventDefault();
-      history.goBack();
-      history.push(backButtonRoute);
-    }
-  }
-
+const Header: React.FC<HeaderProps> = ({ backButtonRoute }) => {
   return (
     <header>
       <div className="logo">
         <img src={icon} alt="Ícone"/>
         <h1>Recomenda<span>Filmes</span></h1>
       </div>
-      <Link to={backButtonRoute} onClick={e => handleBackClick(e)}>
+      <Link to={backButtonRoute} >
         <MdArrowBack size={'6rem'} className="back"/>
       </Link>
     </header>	
--- src/pages/Details/index.tsx
+++ src/pages/Details/index.tsx
@@ -169,7 +169,7 @@ const Details: React.FC = () => {
     }
      
     <div id="details">
-      <Header backButtonRoute="/recomendar" goBack={true}/>
+      <Header backButtonRoute="/recomendar"/>
       
       <div className="header-info-container">
         <img 
